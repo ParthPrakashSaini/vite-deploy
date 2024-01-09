d04a61b3c00d1867c6e3d2294b497740c62ed58c
parth prakash saini <parthprakashsaini99@gmail.com>
2024-01-09 17:26:49 +0530

    modified:   src/components/App.jsx 	modified:   src/components/Note.css 	modified:   src/components/Note.jsx 	new file:   src/notes.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
   return (
     <>
       <Header />
-      <Note />
+      {notes.map((card) => {
+        return (
+          <Note key={card.key} title={card.title} content={card.content} />
+        );
+      })}
       <Footer />
     </>
   );
